acc3fcc2fc7721637102d5db7e45e604357b0cff
Guilherme Ramos <guiramos@gmail.com>
2023-10-08 17:15:10 -0400

    Adding custom colors and custom css for notifications(later use)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -7,7 +8,51 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkslategray: "#1b3358",
+        white: "#fff",
+        black: "#000",
+        dimgray: "#525252",
+        lightsalmon: "rgba(241, 145, 109, 0.2)",
+        gray: "#1c1c1c",
+        lavender: "#f0edff",
+      },
+      spacing: {},
+      fontFamily: {
+        poppins: "Poppins",
+      },
+      borderRadius: {
+        "mid-7": "17.7px",
+      },
+      // For light theme as before
+      backgroundColor: {
+        'primary': '#F5D7DB',
+        'secondary': '#F1916D',
+        'accent': '#06162E'
+      },
+      textColor: {
+        'primary': '#06142E',
+        'secondary': '#1B3358',
+      },
+      // For dark theme
+      dark: {
+        backgroundColor: {
+          'primary': '#06142E',
+          'secondary': '#1B3358',
+          'accent': '#F1916D'
+        },
+        textColor: {
+          'primary': '#F5D7DB',
+          'secondary': '#F1916D',
+        },
+      }
+    },
+    fontSize: {
+      "mid-7": "17.7px",
+      "sm-3": "13.3px",
+      inherit: "inherit",
+    },
   },
   variants: {
     extend: {
